64f542052f6f669f503f19433a79322b7ca9f9c1
Virtue Zhao <zd277607529@gmail.com>
2022-01-23 12:58:19 +1300

    update

--- Case_Study/Case_Study_Data_Generator/RandomRBF_Generator.py
+++ Case_Study/Case_Study_Data_Generator/RandomRBF_Generator.py
@@ -6,74 +6,6 @@ from skmultiflow.data import ConceptDriftStream
 from skmultiflow.trees import HoeffdingAdaptiveTreeClassifier
 from sklearn.metrics import accuracy_score
 #%%%
-target_large_abrupt_generator = ConceptDriftStream(
-    stream=RandomRBFGeneratorDrift(model_random_state=2, sample_random_state=2, change_speed=0.2, num_drift_centroids=25),
-    drift_stream=ConceptDriftStream(
-        stream=RandomRBFGeneratorDrift(model_random_state=4, sample_random_state=4, change_speed=0.2, num_drift_centroids=30),
-        drift_stream=RandomRBFGeneratorDrift(model_random_state=6, sample_random_state=6, change_speed=0.2, num_drift_centroids=40),
-        random_state=2, position=5000, width=1
-    ),
-    random_state=0, position=5000, width=1
-)
-
-stream = target_large_abrupt_generator.next_sample(15000)
-
-
-drift_point = [5000, 10000]
-stream_x_all = stream[0]
-stream_y_all = stream[1]
-sample_pass = 0
-sample_num = 10
-
-HT_WithoutDD = HoeffdingAdaptiveTreeClassifier(random_state=42)
-HT_WithDD = HoeffdingAdaptiveTreeClassifier(random_state=42)
-HT_TDM = HoeffdingAdaptiveTreeClassifier(random_state=42)
-
-true_labels = []
-pred_withoutDD = []
-pred_withDD = []
-pred_TDM = []
-accuracy_withoutDD = []
-accuracy_withDD = []
-accuracy_TDM = []
-
-output_stream = []
-for i in range(len(stream_x_all)):
-    data = np.hstack([stream_x_all[i], stream_y_all[i]])
-    output_stream.append(data)
-    x = np.array([data[0:-1]])
-    y = np.array([data[-1]])
-
-    true_labels.append(y[0])
-    pred_withoutDD.append(HT_WithoutDD.predict(x)[0])
-    pred_withDD.append(HT_WithDD.predict(x)[0])
-    pred_TDM.append(HT_TDM.predict(x)[0])
-
-    HT_WithoutDD.partial_fit(x, y)
-    HT_WithDD.partial_fit(x, y)
-    HT_TDM.partial_fit(x, y)
-
-    if (sample_pass + 1) % sample_num == 0:
-        accuracy_withoutDD.append(accuracy_score(true_labels, pred_withoutDD))
-        accuracy_withDD.append(accuracy_score(true_labels, pred_withDD))
-        accuracy_TDM.append(accuracy_score(true_labels, pred_TDM))
-    if sample_pass in drift_point:
-        HT_WithDD = HoeffdingAdaptiveTreeClassifier(random_state=42)
-        HT_WithDD.partial_fit(x, y)
-        if sample_pass == drift_point[1]:
-            HT_TDM = HoeffdingAdaptiveTreeClassifier(random_state=42)
-            HT_TDM.partial_fit(x, y)
-
-    sample_pass += 1
-
-sns.set()
-# plt.plot(accuracy_withoutDD[30:], label=['Without DD'])
-plt.plot(accuracy_withDD[30:], label=['With DD'])
-plt.plot(accuracy_TDM[30:], label='TDM')
-plt.legend()
-plt.show()
-print(np.count_nonzero(stream_y_all))
-#%%%
 def generate_data(concept_size, drift_type):
 
     if concept_size == 'Large':
@@ -87,12 +19,12 @@ def generate_data(concept_size, drift_type):
     print("Generating RandomRBF - " + concept_size +'(' + str(example_num) + ')' + "-" + drift_type + '(' + str(drift_width) + ')')
     data_generator = ConceptDriftStream(
         stream=RandomRBFGeneratorDrift(model_random_state=2, sample_random_state=2, change_speed=0.2,
-                                       num_drift_centroids=15),
+                                       num_drift_centroids=25),
         drift_stream=ConceptDriftStream(
             stream=RandomRBFGeneratorDrift(model_random_state=4, sample_random_state=4, change_speed=0.2,
-                                           num_drift_centroids=20),
+                                           num_drift_centroids=30),
             drift_stream=RandomRBFGeneratorDrift(model_random_state=6, sample_random_state=6, change_speed=0.2,
-                                                 num_drift_centroids=40),
+                                                 num_drift_centroids=45),
             random_state=2, position=5000, width=1
         ),
         random_state=0, position=5000, width=1
